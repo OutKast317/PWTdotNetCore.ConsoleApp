37d1662e143ec56ef8d6d3a230f1fd7f4dbb5953
OutKast <phonewinttun173@gmail.com>
2024-10-25 20:10:18 +0630

    feat: ado.net create.edit,update

--- PWTdotNetCore.ConsoleApp/Program.cs
+++ PWTdotNetCore.ConsoleApp/Program.cs
@@ -1,2 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using PWTdotNetCore.ConsoleApp;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+
+
+AdoDotNetExample adoDotNetExample = new AdoDotNetExample();
+//adoDotNetExample.Read();
+//adoDotNetExample.Create();
+//adoDotNetExample.Edit();
+adoDotNetExample.Update();
+
+Console.ReadKey();
